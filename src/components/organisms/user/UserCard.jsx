80d0171ec0b3542febf5e3efb8cb323fec2a85e6
tkm0805 <61465998+tkm0805@users.noreply.github.com>
2022-05-24 01:12:38 +0900

    Initial commit

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>1111@aaa.com</dd>
+        <dt>TEL</dt>
+        <dd>000-0000-0000</dd>
+        <dt>会社名</dt>
+        <dd>ああああ会社</dd>
+        <dt>WEB</dt>
+        <dd>httttttt.com</dd>
+      </dl>
+    </div>
+  );
+};
